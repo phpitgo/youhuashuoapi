9e699e93a071f05d1b026b48c193a05dadd6dcf2
刘海亮 <you@example.com>
2018-12-29 16:28:20 +0800

    index time and content limit

--- app/Model/Article.php
+++ app/Model/Article.php
@@ -3,8 +3,21 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
 
 class Article extends Model
 {
+	protected $guarded = [];
+    protected $appends = ['create_time', 'sub_content'];
     //
+    //
+    public function getCreateTimeAttribute()
+    {
+    	return Carbon::createFromFormat('Y-m-d H:i:s', $this->attributes['created_at'])->diffForHumans();
+    }
+
+    public function getSubContentAttribute()
+    {
+    	return str_limit($this->attributes['content']);
+    }
 }
