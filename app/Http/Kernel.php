204c3886f0dd209bfa892a1ef3639fd3457d42ff
刘海亮 <you@example.com>
2019-01-04 11:17:56 +0800

    register middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -60,6 +60,9 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+
+        // 开发添加
+        'switchguard' => \App\Http\Middleware\SwitchGuard::class,
     ];
 
     /**
